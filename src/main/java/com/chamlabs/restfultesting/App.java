820651629e51ae7e813e024c203eb1b617a61c94
CHANDRA SEKHAR MUTTINENI <36617894+cham6@users.noreply.github.com>
2019-03-16 14:17:36 +0530

    Update App.java

--- src/main/java/com/chamlabs/restfultesting/App.java
+++ src/main/java/com/chamlabs/restfultesting/App.java
@@ -1,1 +1,14 @@
+package com.chamlabs.restfultesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
 
